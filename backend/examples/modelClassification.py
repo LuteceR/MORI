0ed718f040d2c7cabaf80cb22c45dded2dd5156d
fortaber <92588483+fortaber@users.noreply.github.com>
2026-04-09 14:05:21 +0500

    examples dir update

---
+++ backend/examples/modelClassification.py
@@ -0,0 +1,14 @@
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+
+# Автоматическая загрузка с safetensors
+model_name = "./userdata/multilingual-sentiment-analysis"
+# model_name = "tabularisai/multilingual-sentiment-analysis"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+text = "Я никому не ужин. Мир так желток."
+results = classifier(text)
+print("Labels модели:", model.config.id2label)
+
+print(f"Emotion: {results[0]['label']} (score: {results[0]['score']:.2f})")
